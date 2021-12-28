41feb0f4d6783a61305bbd0ac2b753330627102d
Nikita Leshchev <nikita.leshchev@paybis.com>
2021-12-28 19:48:23 +0300

    Add custom header stamp support

--- tests/SerializerTest.php
+++ tests/SerializerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Happyr\MessageSerializer;
 
+use Happyr\MessageSerializer\CustomHeaderStamp;
 use Happyr\MessageSerializer\Hydrator\ArrayToMessageInterface;
 use Happyr\MessageSerializer\Serializer;
 use Happyr\MessageSerializer\Transformer\MessageToArrayInterface;
@@ -69,6 +70,41 @@ final class SerializerTest extends TestCase
         self::assertEquals(2, $redeliveryStamp->getRetryCount());
     }
 
+    public function testDecodeWithCustomHeaderStamp(): void
+    {
+        $transformer = $this->getMockBuilder(MessageToArrayInterface::class)->getMock();
+        $hydrator = $this->getMockBuilder(ArrayToMessageInterface::class)
+            ->getMock();
+
+        $payload = [
+            'a' => 'b',
+        ];
+        $data = [
+            'body' => \json_encode(
+                array_merge($payload, ['_meta' => ['retry-count' => 2]]),
+                JSON_THROW_ON_ERROR, 512),
+            'headers' => [
+                'custom-header' => 'header-value',
+            ]
+        ];
+
+        $hydrator->expects(self::once())
+            ->method('toMessage')
+            ->with($payload)
+            ->willReturn(new \stdClass());
+
+        $serializer = new Serializer($transformer, $hydrator);
+        $output = $serializer->decode($data);
+
+        self::assertInstanceOf(\stdClass::class, $output->getMessage());
+        /** @var RedeliveryStamp $redeliveryStamp */
+        $redeliveryStamp = $output->last(RedeliveryStamp::class);
+        self::assertEquals(2, $redeliveryStamp->getRetryCount());
+        /** @var CustomHeaderStamp $customHeader */
+        $customHeader = $output->last(CustomHeaderStamp::class);
+        self::assertEquals('header-value', $customHeader->getValue());
+    }
+
     public function testEncode()
     {
         $transformer = $this->getMockBuilder(MessageToArrayInterface::class)
@@ -89,6 +125,32 @@ final class SerializerTest extends TestCase
         self::assertArrayHasKey('headers', $output);
         self::assertArrayHasKey('Content-Type', $output['headers']);
         self::assertEquals('application/json', $output['headers']['Content-Type']);
+
+        self::assertArrayHasKey('body', $output);
+        self::assertEquals(\json_encode(['foo' => 'bar', '_meta' => []]), $output['body']);
+    }
+
+    public function testEncodeWithCustomHeaderStamp()
+    {
+        $transformer = $this->getMockBuilder(MessageToArrayInterface::class)
+            ->setMethods(['toArray'])
+            ->getMock();
+        $hydrator = $this->getMockBuilder(ArrayToMessageInterface::class)->getMock();
+
+        $envelope = (new Envelope(new \stdClass('foo')))->with(new CustomHeaderStamp('custom-header', 'header-value'));
+
+        $transformer->expects(self::once())
+            ->method('toArray')
+            ->with($envelope)
+            ->willReturn(['foo' => 'bar']);
+
+        $serializer = new Serializer($transformer, $hydrator);
+        $output = $serializer->encode($envelope);
+
+        self::assertArrayHasKey('headers', $output);
+        self::assertArrayHasKey('Content-Type', $output['headers']);
+        self::assertEquals('application/json', $output['headers']['Content-Type']);
+        self::assertEquals('header-value', $output['headers']['custom-header']);
 
         self::assertArrayHasKey('body', $output);
         self::assertEquals(\json_encode(['foo' => 'bar', '_meta' => []]), $output['body']);
